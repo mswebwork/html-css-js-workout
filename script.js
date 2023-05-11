3799a17bf67e18ea64ba643c76b369bb13ce7b91
MSX <mszyszka.web@gmail.com>
2023-05-12 00:08:16 +0200

    feat: create fun with random change border color

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello from Nerdbord!')
+console.log('Hello from Nerdbord!');
+
+const colors = ['red', 'black', 'orange', 'blue'];
+const boxBorder = document.querySelector('.box');
+
+function getRandomInt(max) {
+    return  Math.floor(Math.random() * 4);
+}
+function changeBorderColor() {
+    boxBorder.style.borderColor = colors[getRandomInt(4)];
+}
+
+setInterval(changeBorderColor, 3000);
