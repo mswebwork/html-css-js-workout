e6c1fca1b29232126ad7ab7bd24d0363e210919c
MSX <mszyszka.web@gmail.com>
2023-05-12 00:13:36 +0200

    feat: change static numer in fun getRandomInt to colors.length and change name variable to represent Element not string

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const colors = ['red', 'black', 'orange', 'blue'];
 const boxBorder = document.querySelector('.box');
 
 function getRandomInt(max) {
-    return  Math.floor(Math.random() * 4);
+    return  Math.floor(Math.random() * max);
 }
 function changeBorderColor() {
-    boxBorder.style.borderColor = colors[getRandomInt(4)];
+    boxBorder.style.borderColor = colors[getRandomInt(colors.length)];
 }
 
 setInterval(changeBorderColor, 3000);
